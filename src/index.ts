9bd51d519732e4969b3f4faceb50f0e4c437859c
Dylan Pyle <me@dylanpyle.com>
2021-02-24 19:36:45 -0500

    more prompts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,21 @@ import { sha256 } from "./deps.ts";
 
 console.log('Loading words...');
 const words = await Deno.readTextFile("./data/words");
-const wordList = words.split('\n');
+const wordList = words.toLowerCase().split('\n');
 const length = wordList.length;
-console.log(`Got ${length} words`);
+
+const adverbs = wordList.filter(a => a.match(/ly$/));
+const adverbsLength = adverbs.length;
+console.log(`Got ${length} words, ${adverbsLength} adverbs`);
 
 function pickRandom(): string {
   const index = Math.floor(Math.random() * length)
   return wordList[index];
+}
+
+function randomAdverb(): string {
+  const index = Math.floor(Math.random() * adverbsLength)
+  return adverbs[index];
 }
 
 function test(maybe: string) {
@@ -25,18 +33,37 @@ function test(maybe: string) {
 }
 
 while (true) {
-  const [w1, w2] = [pickRandom(), pickRandom()];
-  const [l1, l2] = [w1.toLowerCase(), w2.toLowerCase()];
-  test(`${w1} ${w2}`);
+  const [l1, l2] = [pickRandom(), pickRandom()];
+  const [u1, u2] = [l1.toUpperCase(), l2.toUpperCase()];
+  const [a1, a2] = [randomAdverb(), randomAdverb(), randomAdverb()];
+
+  test(`${u1} ${u2}`);
   test(`${l1} ${l2}`);
-  test(`${w1} ${w2}!`);
+  test(`${u1} ${u2}!`);
   test(`${l1} ${l2}!`);
-  test(`${w1} ${w2}.`);
+  test(`${u1} ${u2}.`);
   test(`${l1} ${l2}.`);
-  test(`${w1}? ${w2}!`);
+  test(`${u1}? ${u2}!`);
   test(`${l1}? ${l2}!`);
-  test(`${w1}, ${w2}!`);
+  test(`${u1}, ${u2}!`);
   test(`${l1}, ${l2}!`);
-  test(`${w1}, ${w2}.`);
+  test(`${u1}, ${u2}.`);
   test(`${l1}, ${l2}.`);
+
+  test(`${u1} ${u2}. how lovely`);
+  test(`a small ${l1}`);
+  test(`a small ${l2}`);
+  test(`language, code, luck, and ${l1}`);
+  test(`language, code, luck, and ${l2}`);
+  test(`standing on the shoulders of ${l1}`);
+  test(`standing on the shoulders of ${l2}`);
+  test(`gently,\n${l1}`);
+  test(`gently,\n${l2}`);
+  test(`slowly,\n${l1}`);
+  test(`slowly,\n${l2}`);
+
+  test(`${a1},\n${a2},\n${l1}`);
+  test(`${a1},\n${a2},\n${l2}`);
+  test(`${a1},\n${a2},\n${l1}.`);
+  test(`${a1},\n${a2},\n${l2}.`);
 }
